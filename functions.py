37b8b8ce1d57d6621663721d7d9dba3b93e39f74
TeoSkondras <t8200156@aueb.gr>
2023-05-07 15:02:19 +0300

    Debug functions and test them

--- functions.py
+++ functions.py
@@ -69,10 +69,11 @@ event_id = 0
 
 # updates the events_log table to emit a new event
 def update_events_log(event_id, user_id, event_type, timestamp):
-    SQL = "INSERT INTO events_log (event_id, userID, event_type, timestamp) VALUES ({event_id}, {userID}, {event_type}, {timestamp})".format(
-        event_id=event_id, userId=user_id , event_type=event_type , timestamp=timestamp
+    SQL = "INSERT INTO events_log (event_id, userID, event_type, timestamp) VALUES ({event_id}, {userID}, {event_type}, NOW())".format(
+        event_id=event_id, userID=user_id , event_type=event_type , timestamp=timestamp
     )
     cursor_events_log.execute(SQL)
+    db_events_log.commit()
 
 # -------REDIS UTILS-------
 
@@ -90,6 +91,7 @@ def is_user_allowed_to_join_meeting(user_id , meeting_id):
         if meeting[0] == meeting_id:
             # if public (audience = '') anyone can join so return true
             if meeting[4] == '':
+                
                 return True
             else:
                 # else check if the users email is contained in the audience comma separated string
@@ -102,26 +104,41 @@ def is_user_allowed_to_join_meeting(user_id , meeting_id):
                         else:
                             return False 
 
+def is_user_already_in_the_meeting_instance(user_id, meeting_id, order_id):
+    key = "meetingId:" + str(meeting_id) + ":orderId:" + str(order_id) + ":participants"
+    meeting_participants_ids = r.lrange(key , 0 , -1 )
+    if str(user_id) in meeting_participants_ids:
+        return True
+
+def initialize_event_id():
+    r.set('currentEventID',0)
+
 # --------- 1: A user joins an active meeting instance ---------
 # KEY: "meetingId:int:orderID:int:participants" VALUE: [userId1 , userId2 , ... ]
 def join_meeting(user_id, meeting_id, order_id):
     # check if the meeting instance the user tries to join is active
     # construct the string to look up
     meeting_instance_id = str(meeting_id) + " " + str(order_id)
+    event_id = int(r.get('currentEventID'))
     # get a list of active meeting instaces from redis
     active_meetings_from_scheduler = r.lrange("active_meetings" , 0 , -1 )
     if meeting_instance_id in active_meetings_from_scheduler:
         print("Meeting Active. Trying to join....")
         # check if the user is allowed to join the meeting
         if is_user_allowed_to_join_meeting(user_id , meeting_id):
-            key = "meetingId:" + str(meeting_id) + ":orderId:" + str(order_id) + ":participants"
-            value = str(user_id)
-            print(key + " " + value)
-            r.lpush(key , value)
-            print("User: " +  str(user_id) + " joined meeting instance: " + str(meeting_instance_id) )
-            # Emit join to events_log
-            event_id += 1
-            update_events_log(event_id , user_id , Event_types.JOIN.value , datetime.datetime.now())
+            if not is_user_already_in_the_meeting_instance(user_id, meeting_id, order_id):
+                key = "meetingId:" + str(meeting_id) + ":orderId:" + str(order_id) + ":participants"
+                value = str(user_id)
+                print(key + " " + value)
+                r.lpush(key , value)
+                print("User: " +  str(user_id) + " joined meeting instance: " + str(meeting_instance_id) )
+                # Emit join to events_log
+                event_id +=1
+                r.set('currentEventID',event_id)
+                print(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+                update_events_log(event_id , user_id , Event_types.JOIN.value , datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+            else:
+                print("User: " +  str(user_id) + " has already joined the meeting: " + str(meeting_id))
         else:
             print("User: " +  str(user_id) + " not allowed to join meeting: " + str(meeting_id) )
     else:
@@ -135,10 +152,12 @@ def leave_meeting(user_id, meeting_id, order_id):
     meeting_instance_id = str(meeting_id) + " " + str(order_id)
     key = "meetingId:" + str(meeting_id) + ":orderId:" + str(order_id) + ":participants"
     meeting_participants = r.lrange(key , 0 , -1 )
+    event_id = int(r.get('currentEventID'))
     # if the user is in the participants list remove him
-    if user_id in meeting_participants:
+    if str(user_id) in meeting_participants:
         r.lrem(key , 0 , user_id)
         event_id += 1
+        r.set('currentEventID',event_id)
         # update events_log
         update_events_log(event_id , user_id , Event_types.LEAVE.value , datetime.datetime.now())
         print("User: " +  str(user_id) + " left meeting instance: " + str(meeting_instance_id) )
@@ -160,13 +179,21 @@ def show_active_meetings():
 def empty_participants_from_finished_meeting(meeting_id , order_id):
     key = "meetingId:" + str(meeting_id) + ":orderId:" + str(order_id) + ":participants"
     meeting_participants_ids = r.lrange(key , 0 , -1 )
+    event_id = int(r.get('currentEventID'))
     for user_id in meeting_participants_ids:
         event_id += 1
+        r.set('currentEventID',event_id)
         update_events_log(event_id , int(user_id) , Event_types.TIMEOUT.value , datetime.datetime.now())
     r.delete(key)
 
 
-
 # -------TEST MAIN FUNCTIONS -------
+initialize_event_id()
 show_active_meetings()
-join_meeting(user_id=1 , meeting_id=1 , order_id=4)
+show_meeting_participants(1,1)
+join_meeting(user_id=1 , meeting_id=1 , order_id=1)
+join_meeting(user_id=2 , meeting_id=1 , order_id=1)
+join_meeting(user_id=3 , meeting_id=1 , order_id=1)
+leave_meeting(user_id=1 , meeting_id=1 , order_id=1)
+empty_participants_from_finished_meeting(1,1)
+show_meeting_participants(1,1)
